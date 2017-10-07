27d9bac71d876d58726da9ef75b558ce9582799c
Adam Fudge <coolfudge@gmail.com>
2017-10-07 22:22:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.scan(/[aeiou]+\w/)
+  if word.scan(/[aeiou]+\w/) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
