4d457f2723e703cddba98145d49a00f1f5ba3a19
Adam Fudge <coolfudge@gmail.com>
2017-10-07 22:35:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if word.match(//) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if word.match(//) != nil
+    return true
+  else
+    return false
+  end
 end
